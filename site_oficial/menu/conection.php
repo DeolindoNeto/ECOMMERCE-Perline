2a83eb22a7ee582ae7e4e81854601b282a1fdfba
Deoola <91548957+Deoola@users.noreply.github.com>
2022-08-29 11:10:31 -0300

    UPDATE HOME

---
+++ site_oficial/menu/conection.php
@@ -0,0 +1,16 @@
+<?php
+
+    $conecta = pg_connect("host=pgsql.projetoscti.com.br
+    port=5432
+    dbname=projetoscti13
+    user=projetoscti13
+    password=0812152830");
+    
+    if (!$conecta) {
+        echo '<script language="javascript">';
+        echo "alert('Não foi possível estabelecer conexão com o banco de dados!')";
+        echo '</script>';
+        exit;
+    }
+     
+?>
