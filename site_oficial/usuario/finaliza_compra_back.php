b457708af18ad7b4b89286067f82d7bba82c2e45
Deoola <91548957+Deoola@users.noreply.github.com>
2022-10-23 18:43:17 -0300

    commit

--- site_oficial/usuario/finaliza_compra_back.php
+++ site_oficial/usuario/finaliza_compra_back.php
@@ -5,30 +5,35 @@
     $compraFinalizada = FALSE;
     $id_user = $_SESSION['usuariologado']['id_user'];
 
-   function validarProdutos($conecta, $resultado_lista)
+   function validarProdutos($conecta, $resultado_lista, $qtdeVendida)
     {
-    //    if($resultado_lista)
-    //    {
-    //     foreach($resultado_lista as $linha)
-    //     {
-    //         $id_produto=$linha["id_produto"];
-    //         $sql="SELECT * FROM produto WHERE id_produto = $id_produto;";
+        if($resultado_lista)
+        {
+         foreach($resultado_lista as $linha)
+         {
+             $id_produto=$linha["id_produto"];
+             $sqlPegaProd="SELECT * FROM produto WHERE id_produto = $id_produto;";
+             $res= pg_query($conecta, $sqlPegaProd);
 
-    //         $res= pg_query($conecta, $sql); 
-    //        if($res<=0) 
-    //            return false;   
-    //     } 
+             if($res<=0) 
+                return false;
 
-    //   }
+             /* if($linha['quantidade'] < $qtdeVendida){
+                echo "<br><br><br><br>aqui"."$estoque";
+                echo "<script type='text/javascript'>alert('Desculpe, não temos a quantidade pedida em estoque!!')</script>";
+             }
+             else if ($linha['quantidade'] >= $qtdeVendida){
+                return true;
+             }     VALIDAÇÃO DE ESTOQUE E QUANTIDADE EM CARRINHOOOOO*/ 
+         }         
         return true;
-    } 
-   
+        } 
+    }
     function atualizarEstoque($conecta, $id_produto, $qtdVendida)
     {
            $sql =" UPDATE produto
            set quantidade=quantidade-$qtdVendida
              where id_produto=$id_produto;";
-            echo $sql;
         $res=pg_query($conecta, $sql);
         if($res<=0) 
                return false; 
@@ -38,7 +43,12 @@
     
     $id_produto = $_SESSION['carrinho'];
     
-   validarProdutos($conecta, $resultado_lista); 
+    foreach($resultado_lista as $linha)
+    {
+        $qtdeVendida = $linha['qtde'];
+        validarProdutos($conecta, $resultado_lista, $qtdeVendida); 
+    }
+
     $sql = " INSERT INTO venda (id_venda, id_user, data_hora_venda, observacoes, excluido) VALUES (nextval('venda_id_venda_seq'::regclass), $id_user, current_timestamp,'Venda realizada', false);";
     $res = pg_query($conecta, $sql);
     $qtdLinhas = pg_affected_rows($res);
@@ -51,10 +61,7 @@
             $preco = $linha['preco'];
             $qtdVendida = $linha['qtde'];
             $id_produto = $linha['id_produto'];
-            //$id_venda = $linha['id_venda'];
             $sql = " INSERT INTO itemvenda (id_venda, id_produto, qtde, valor_unit) VALUES (CURRVAL('venda_id_venda_seq'),".$id_produto.",".$qtdVendida.",".$preco." );";
-            echo $sql;
-
             $res = pg_query($conecta, $sql);
     
             atualizarEstoque($conecta, $id_produto, $qtdVendida);
@@ -68,4 +75,3 @@
     // Fecha a conexão com o PostgreSQL
     pg_close($conecta);
 ?>
-
