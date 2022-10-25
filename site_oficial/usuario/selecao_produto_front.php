9d20d279b88ea07189914b8fe50826d7e516a319
Deoola <91548957+Deoola@users.noreply.github.com>
2022-10-24 22:35:16 -0300

    commit

--- site_oficial/usuario/selecao_produto_front.php
+++ site_oficial/usuario/selecao_produto_front.php
@@ -130,7 +130,7 @@
                     <div class='imgproduto' >
                         <br>
                         <a href='../usuario/selecao_detalhes_front.php?id_produto=".$linha['id_produto']."&imgprod=".$linha['campo_imagem']."'> 
-                        <img src='../img/$imagemproduto' width='100px' heigth='100px'>
+                        <img src='../img/$imagemproduto' width='100px' heigth='75px'>
                         </a>
                     </div>
 
